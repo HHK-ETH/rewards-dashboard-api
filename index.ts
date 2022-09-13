6b215524a250c7826a5af6553e94337242d3fd81
HHK <hhk.contact@protonmail.com>
2022-09-13 18:12:55 +0200

    feat: add auth middleware

--- index.ts
+++ index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
+import { fetchPendingSushiRoutine, StorageHelper } from './src';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const port = 3333;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const authMiddleware = async function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): Promise<void> {
+  const token = req.headers.authorization;
+  if (!token || token.slice(7) !== process.env.TOKEN) {
+    res.status(401).end();
+    return;
+  }
+  next();
+};
+
+const storageHelper = StorageHelper.getInstance();
+setTimeout(() => {
+  fetchPendingSushiRoutine(storageHelper);
+}, 900_000);
+
+app.get('/api', authMiddleware, async (req, res) => {
+  res.json(await storageHelper.read());
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
