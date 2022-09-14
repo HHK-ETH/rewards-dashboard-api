152ee65d98dc9fe42b76ece1a4baed4dfba65282
HHK <hhk.contact@protonmail.com>
2022-09-14 20:51:12 +0200

    feat: update storage types

--- src/storageHelper.ts
+++ src/storageHelper.ts
@@ -1,27 +1,6 @@
 import fs from 'fs';
 import fsPromise from 'fs/promises';
-import { ChainId, MINICHEF_ADDRESS } from './constants';
-
-type Storage = {
-  [label: string]: {
-    rewards: {
-      sushiRewards: number;
-      tokenRewards: {
-        [address: string]: {
-          rewards: number;
-          amount: number;
-          token: string;
-          tokenName: string;
-          rewardPerSecond: number;
-          pingedRefill: boolean;
-          notify: boolean;
-        };
-      };
-    };
-    amount: number;
-    pingedRefill: boolean;
-  };
-};
+import { MINICHEF_ADDRESS, Rewarder } from './constants';
 
 export default class StorageHelper {
   private static instance: StorageHelper;
@@ -31,8 +10,8 @@ export default class StorageHelper {
       if (!err) return;
       const storage: any = {};
       for (const id in MINICHEF_ADDRESS) {
-        const label = ChainId[id as any];
-        storage[label] = { rewards: { sushiRewards: 0, tokenRewards: {} }, amount: 0, pingedRefill: false };
+        const label = id;
+        storage[label] = [];
       }
       fs.writeFile('./src/storage.json', JSON.stringify(storage), (err) => {
         if (!err) return;
@@ -49,12 +28,12 @@ export default class StorageHelper {
     return StorageHelper.instance;
   }
 
-  public async read(): Promise<Storage> {
+  public async read(): Promise<{ [chainId: number]: Rewarder[] }> {
     const content = await fsPromise.readFile('./src/storage.json', 'utf-8');
     return JSON.parse(content);
   }
 
-  public async write(content: Storage): Promise<void> {
+  public async write(content: { [chainId: number]: Rewarder[] }): Promise<void> {
     await fsPromise.writeFile('./src/storage.json', JSON.stringify(content));
   }
 }
